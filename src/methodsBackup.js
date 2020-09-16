7390f505a8db0f02537b70b3c08dcf24f49c95be
jc-reporting-stuff <68667230+jc-reporting-stuff@users.noreply.github.com>
2020-09-16 14:44:47 -0400

    updated method backup

--- src/methodsBackup.js
+++ src/methodsBackup.js
@@ -29,7 +29,7 @@ const CHEM162 = {
     {
       name: "QM-S Q1807",
       rangesLow: [2.23, 0.747, 3.47, 1.03, 0.913, 0.119, 1.11, null],
-      rangesHigh: [3.2, 0.851, 4.3, 1.16, 1.16, 0.152, 1.73, null]
+      rangesHigh: [3.2, 0.851, 4.3, 1.22, 1.16, 0.152, 1.73, null]
     },
     {
       name: "QM-B Q1720",
@@ -39,43 +39,274 @@ const CHEM162 = {
   ]
 };
 
-const CHEM055 = {
-  name: "CHEM-055",
-  description: "Metals in Tissues - DO NOT USE YET",
-  elements: ["Mn", "Fe", "Co", "Cu", "Zn", "Se", "Mo", "Pb"],
-  units: ["ppb", "ppm", "ppb", "ppm", "ppm", "ppm", "ppb", "ppm"],
-  checkStdTolerance: 0.1,
+const CHEM114 = {
+  name: "CHEM-114",
+  description: "Metals in soils, biosolids and wastewaters",
+  elements: ["Cr", "Co", "Ni", "Cu", "Zn", "As", "Se", "Mo", "Cd", "Hg", "Pb"],
+  units: ["ppm", "ppm", "ppm", "ppm", "ppm", "ppm", "ppm", "ppm"],
+  checkStdTolerance: 0.15,
   checkStds: [
     {
-      name: "5/50 ppb",
-      expectedValues: [5, 50, 5, 50, 50, 5, 5, 5]
+      name: "Calibration Check",
+      expectedValues: [10, 10, 10, 50, 50, 10, 10, 10, 10, 1, 10]
     }
   ],
   blanks: [
     {
-      name: "Serum Blank",
-      type: "serum",
-      LOQs: [0.9, 0.013, 0.3, 0.0008, 0.0011, 0.007, 1, null]
+      name: "Solids Blank",
+      type: "solids",
+      LOQs: [null, null, null, null, null, null, null, null, null, null, null]
     },
     {
-      name: "Blood Blank",
-      type: "blood",
-      LOQs: [null, null, null, null, null, 0.029, null, 0.001]
+      name: "Water Blank",
+      type: "waters",
+      LOQs: [null, null, null, null, null, null, null, null, null, null, null]
     }
   ],
   duplicateTolerance: 15,
-  calStandards: [0.05, 0.1, 0.25, 0.5, 1, 5, 10, 50, 250],
+  calStandards: [0.05, 0.1, 0.5, 1, 5, 10, 50],
   sigFigs: 3,
   referenceMaterials: [
     {
-      name: "QM-S Q1807",
-      rangesLow: [2.6, 0.7, 3.7, 0.9, 0.9, 0.1, 1.0, null],
-      rangesHigh: [3.1, 1.0, 4.2, 1.2, 1.2, 0.15, 1.7, null]
+      name: "Ref 2b",
+      rangesLow: [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      rangesHigh: [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    }
+  ]
+};
+
+const CHEM055 = {
+  name: "CHEM-055",
+  description: "Metals in tissues",
+  elements: [
+    "Be",
+    "B",
+    "Mg",
+    "V",
+    "Cr",
+    "Mn",
+    "Fe",
+    "Co",
+    "Ni",
+    "Cu",
+    "Zn",
+    "As",
+    "Se",
+    "Mo",
+    "Cd",
+    "Sn",
+    "Sb",
+    "Ba",
+    "Hg",
+    "Tl",
+    "Pb"
+  ],
+  units: [
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm",
+    "ppm"
+  ],
+  checkStdTolerance: 0.15,
+  checkStds: [
+    {
+      name: "Calibration Check",
+      expectedValues: [
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        1,
+        10,
+        10
+      ]
+    }
+  ],
+  blanks: [
+    {
+      name: "Method Blank",
+      type: "normal",
+      LOQs: [
+        0.008,
+        0.16,
+        4.1,
+        0.04,
+        0.12,
+        0.52,
+        8.6,
+        0.03,
+        0.38,
+        0.46,
+        2.1,
+        0.02,
+        0.07,
+        0.02,
+        0.02,
+        0.5,
+        0.02,
+        0.03,
+        0.003,
+        0.005,
+        0.016
+      ]
+    }
+  ],
+  duplicateTolerance: 20,
+  calStandards: [0.01, 0.05, 0.25, 0.5, 1, 5, 10, 0.25, 0.5, 1, 5, 10, 50],
+  sigFigs: 3,
+  referenceMaterials: [
+    {
+      name: "Bovine Liver",
+      rangesLow: [
+        null,
+        null,
+        459.33,
+        null,
+        null,
+        4.46,
+        122.62,
+        0.14,
+        null,
+        105.9,
+        84.68,
+        null,
+        0.99,
+        3.42,
+        0.0335,
+        null,
+        null,
+        null,
+        null,
+        null,
+        0.06
+      ],
+      rangesHigh: [
+        null,
+        null,
+        558.39,
+        null,
+        null,
+        5.9,
+        177.16,
+        0.2,
+        null,
+        149.82,
+        110.78,
+        null,
+        1.53,
+        4.68,
+        0.06,
+        null,
+        null,
+        null,
+        null,
+        null,
+        0.42
+      ]
     },
     {
-      name: "QM-B Q1720",
-      rangesLow: [null, null, null, null, null, 0.15, null, 0.1],
-      rangesHigh: [null, null, null, null, null, 0.2, null, 0.14]
+      name: "TORT-3",
+      rangesLow: [
+        0.0062,
+        2.74,
+        942.22,
+        7.62,
+        1.09,
+        13.01,
+        148.29,
+        0.87,
+        4.34,
+        416.01,
+        114.4,
+        55.86,
+        9.09,
+        2.93,
+        35.34,
+        null,
+        0.0344,
+        null,
+        0.222,
+        0.0076,
+        0.17
+      ],
+      rangesHigh: [
+        0.0134,
+        3.7,
+        1177.78,
+        9.18,
+        2.35,
+        16.49,
+        186.69,
+        1.11,
+        5.66,
+        512.37,
+        145.6,
+        69.66,
+        10.95,
+        3.65,
+        44.4,
+        null,
+        0.0776,
+        null,
+        0.34,
+        0.0124,
+        0.23
+      ]
     }
   ]
 };
@@ -356,6 +587,6 @@ const TOXI064 = {
   ]
 };
 
-const methods = [CHEM162, CHEM057, TOXI064, CHEM055];
+const methods = [CHEM055, CHEM057, CHEM114, CHEM162, TOXI064];
 
 module.exports = methods;
