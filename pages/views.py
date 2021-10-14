e66e8f2c8b19eab715c358ce5eea186688a8011b
Jeffrey Charters <jcharter@uoguelph.ca>
2021-10-14 15:06:15 -0400

    viz embeds working

--- pages/views.py
+++ pages/views.py
@@ -11,28 +11,34 @@ class HomePageView(TemplateView):
 
 
 def VizHomeView(request):
-    if request.method == 'GET':
-        return render(request, 'get_username.html')
+    username = 'TU1'
+    server_url = 'http://131.104.184.6/trusted'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
+    client_ip = request.META.get('REMOTE_ADDR')
+    data = {'username': username, 'client_ip': client_ip}
+    r = requests.post(
+        server_url,
+        headers=headers,
+        data=data)
 
-    if request.method == 'POST':
+    ticket = r.text
 
-        username = request.POST.get('username')
-        server_url = 'http://131.104.184.6/trusted'
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
-        client_ip = request.META.get('REMOTE_ADDR')
-        data = {'username': username, 'client_ip': client_ip}
-        r = requests.post(
-            server_url,
-            headers=headers,
-            data=data)
+    r = requests.post(
+        server_url,
+        headers=headers,
+        data=data)
 
-        ticket = r.text
+    ticket2 = r.text
 
-        url = f'http://131.104.184.6/trusted/{ticket}/views/TestData/DummyData'
+    # Each viz needs its own ticket.
+    # Abstract this into a function don't ever leave this how it is.
 
-        context = {
-            'ticket': ticket
-        }
-        # return render(request, 'viz_main.html', context=context)
-        return HttpResponseRedirect(url)
+    # url = f'http://131.104.184.6/trusted/{ticket}/views/TestData/DummyData'
+
+    context = {
+        'ticket': ticket,
+        'ticket2': ticket2
+    }
+    return render(request, 'viz_main.html', context=context)
+    # return HttpResponseRedirect(url)
