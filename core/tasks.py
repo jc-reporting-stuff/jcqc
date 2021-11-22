80f3b8a5c07f69df6749405944fc49c0907516a1
Jeffrey Charters <jcharter@uoguelph.ca>
2021-11-22 13:47:48 -0500

    move tasks and modify old sqeuences

--- core/tasks.py
+++ core/tasks.py
@@ -6,9 +6,9 @@ from django.conf import settings
 
 from django.utils import timezone
 from datetime import timedelta
-import time
 
 from accounts.models import User
+from sequences.models import Reaction
 
 
 @shared_task(name='inactivate_dormant_users')
@@ -28,13 +28,17 @@ def remove_old(days=14):
         timezone.now() - timedelta(days=days)).strftime('%s'))
     user_sequence_folders = os.path.join(
         settings.FILES_BASE_DIR, settings.CUSTOMER_SEQUENCES_DIR)
-    files_list = []
+
     for folder_name, subfolders, filenames in os.walk(user_sequence_folders):
         for file in filenames:
-            files_list.append(os.path.join(folder_name, file))
-
-    for file in files_list:
-        # File modified date in epoch time
-        file_modified_date = os.path.getmtime(file)
-        if file_modified_date < inactivation_time:
-            os.remove(file)
+            file_path = os.path.join(folder_name, file)
+            file_modified_date = os.path.getmtime(file_path)
+            if file_modified_date < inactivation_time:
+                try:
+                    sequence_id = int(file.split('_')[0])
+                    reaction = Reaction.objects.get(id=sequence_id)
+                    reaction.status = 'v'
+                    reaction.save()
+                except:
+                    pass
+                os.remove(file_path)
