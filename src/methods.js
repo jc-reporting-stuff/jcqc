0f23cdbff2b379e1c55dd31446d14ff25354c89a
jc-reporting-stuff <68667230+jc-reporting-stuff@users.noreply.github.com>
2022-01-19 13:03:31 -0500

    updat TOXI-064 WF, rm TOXI064JC

--- src/methods.js
+++ src/methods.js
@@ -1,14 +1,14 @@
-import CHEM055 from './methods/CHEM055';
-import CHEM057 from './methods/CHEM057';
-import CHEM114 from './methods/CHEM114';
-import CHEM162 from './methods/CHEM162';
-import TOXI064 from './methods/TOXI064';
-import TOXI064JC from './methods/TOXI064JC';
-import TOXI064Bi from './methods/TOXI064Bi';
-import TOXI064Pb from './methods/TOXI064Pb';
+import CHEM055 from "./methods/CHEM055";
+import CHEM057 from "./methods/CHEM057";
+import CHEM114 from "./methods/CHEM114";
+import CHEM162 from "./methods/CHEM162";
+//import TOXI064 from './methods/TOXI064';
+import TOXI064JC from "./methods/TOXI064JC";
+import TOXI064Bi from "./methods/TOXI064Bi";
+import TOXI064Pb from "./methods/TOXI064Pb";
 // import CHEM055Alltech from './methods/CHEM055Alltech';
-import REEs from './methods/REEs';
-import SerumIodine from './methods/SerumIodine';
+import REEs from "./methods/REEs";
+import SerumIodine from "./methods/SerumIodine";
 
 // Removed CHEM055Alltech
 const methods = [
@@ -16,7 +16,7 @@ const methods = [
   CHEM057,
   CHEM114,
   CHEM162,
-  TOXI064,
+  //TOXI064,
   TOXI064JC,
   TOXI064Bi,
   TOXI064Pb,
